562e9ca08e947b92558760507b33d519d9c97b19
vierrra <vierrra@hotmail.com>
2024-10-09 22:20:14 -0300

    Commit project

--- MoviesMock/ViewController/MovieScreen/MovieScreen.swift
+++ MoviesMock/ViewController/MovieScreen/MovieScreen.swift
@@ -5,4 +5,55 @@
 //  Created by Renato Vieira on 09/10/24.
 //
 
-import Foundation
+import UIKit
+
+class MovieScreen: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 180, height: 300)
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = 10
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .white
+        collectionView.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: MovieCollectionViewCell.identifier)
+        return collectionView
+    }()
+    
+    public func configProtocols(delegate: UICollectionViewDelegateFlowLayout, dataSource: UICollectionViewDataSource) {
+        collectionView.delegate = delegate
+        collectionView.dataSource = dataSource
+    }
+    
+    private func setup() {
+        buildViewHierarchy()
+        configConstraints()
+    }
+    
+    private func buildViewHierarchy() {
+        addSubview(collectionView)
+    }
+    
+    private func configConstraints() {
+        //collectionView.frame = bounds
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor, constant: 15),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
--- MoviesMock/ViewController/MovieViewController.swift
+++ MoviesMock/ViewController/MovieViewController.swift
@@ -1,32 +1,20 @@
 import UIKit
 
-class MovieViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class MovieViewController: UIViewController {
     
-    private var collectionView: UICollectionView?
     private var movies: [Movie] = []
+    private var screen: MovieScreen?
+    
+    override func loadView() {
+        screen = MovieScreen()
+        view = screen
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: view.frame.size.width / 2 - 10, height: view.frame.size.width / 1.5)
-        layout.minimumInteritemSpacing = 5
-        layout.minimumLineSpacing = 10
-        
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView?.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: MovieCollectionViewCell.identifier)
-        collectionView?.dataSource = self
-        collectionView?.delegate = self
-        collectionView?.backgroundColor = .white
-        
-        guard let collectionView = collectionView else {
-            return
-        }
-        
-        view.addSubview(collectionView)
-        collectionView.frame = view.bounds
-        
         loadMovies()
+        screen?.configProtocols(delegate: self, dataSource: self)
     }
     
     private func loadMovies() {
@@ -42,12 +30,15 @@ class MovieViewController: UIViewController, UICollectionViewDataSource, UIColle
             let movieResponse = try decoder.decode(MovieResponse.self, from: data)
             self.movies = Array(movieResponse.results.prefix(10))
             DispatchQueue.main.async {
-                self.collectionView?.reloadData()
+                self.screen?.collectionView.reloadData()
             }
         } catch {
             print("Failed to load movies: \(error)")
         }
     }
+}
+
+extension MovieViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
